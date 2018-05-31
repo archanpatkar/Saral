c12930c0bd363f73b75761cd1f3287c47fd0296f
Archan Patkar <archanpatkar@gmail.com>
2018-05-31 17:49:28 +0530

    Update

---
+++ Backyard.js
@@ -0,0 +1,32 @@
+// let codes = [
+//     [
+//         "begin",
+
+//         [
+//             "define", "f1", [
+//                 "lambda", [],
+//                     [ "invoke" , ["lambda",["x","y"],["print", ["+","x","y"]]] , [10,20] ]
+//             ]
+//         ],
+
+//         // ["lambda", [] , ["print",'"Function Literal!!"']
+
+//         // ["print" , ["f1"]] ,
+
+//         // ["print" , ["f1"]]
+//         // ["f2"],
+
+//         // ["define", "f3", ["f1"]],
+
+//         // ["f3"],
+
+//         ["f1"],
+
+//         ["print" , '"Hello World!"']
+//     ]
+// ];
+
+// for (let e in codes) {
+//     eval(codes[e], env);
+// }
+// console.log(env);
