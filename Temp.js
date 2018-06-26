ee88914b58deacf98aa22d2cc3af065172341bce
Archan Patkar <archanpatkar@gmail.com>
2018-06-26 12:06:12 +0530

    Cluster Testing

---
+++ Temp.js
@@ -0,0 +1,75 @@
+const saral = require("./index");
+
+let code =
+[
+    "begin",
+
+    // ["print" , '"Testing Invoke"'],
+    //
+    // [ "defun" , "f1" , [],
+    //     [ "begin",
+    //         ["print" , '"Entering f1"'],
+    //         [ "invoke" , ["lambda",["x","y"],["print", ["+","x","y"]]] , [10,20] ],
+    //         ["print" , '"Leaving f1"']
+    //     ]
+    // ],
+    //
+    // ["f1"],
+    //
+    // ["define","i",0],
+    //
+    // ["print" , [ "+" , '"i = "' , "i" ] ],
+    //
+    // [ "while" , ["/=","i",10] ,
+    //     [ "begin",
+    //         [ "print",'"Hello"' ],
+    //         [ "update" , "i" , ["++","i"] ]
+    //     ]
+    // ],
+    //
+    // ["define", "l1", ["list",10,20,30,40,50,60]],
+    //
+    // ["for", "i", "in", "l1",
+    //   ["begin",
+    //     ["print","i"]
+    //   ]
+    // ],
+    //
+    // ["define","i",50],
+    //
+    // [
+    //   "if", ["=","i",50],
+    //     ["print",'"SAME!"'],
+    //   "else",
+    //     ["print",'"NOT SAME!"']
+    // ],
+
+    ["define", "C1" ,
+      [ "cluster" ,
+        ["begin",
+          ["defun","f1",["x","y"],
+            ["begin",
+              ["print",'"F2!"'],
+              ["print",["+", '"X = "' , "x"]],
+              ["print",["+", '"Y   = "' , "y"]]
+            ]
+          ]
+        ]
+      ]
+    ],
+
+    ["C1", ["f1",10,20]]
+
+    // ["declu", "C1"
+    //   ["begin",
+    //     ["defun","f1",[]
+    //       ["begin",
+    //         ["print",'"F2!"']
+    //       ]
+    //     ]
+    //   ]
+    // ]
+
+];
+
+saral(code);
